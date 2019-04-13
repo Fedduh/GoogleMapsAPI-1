e4ab367c588d3624e23233b243aae453353ae494
Matt Hamers <development@matthamers.com>
2019-04-13 11:51:07 +0200

    starter code map tutorial

---
+++ map.js
@@ -0,0 +1,16 @@
+// STEP 1 create a funciton to initialize the map.
+
+
+// STEP 2 add options to the map.
+
+
+// STEP 3 set first marker.
+
+
+// STEP 4 make function to add markers dynamically.
+
+
+// STEP 5 call marker function with an array of marker objects.
+
+
+// BONUS map styling
