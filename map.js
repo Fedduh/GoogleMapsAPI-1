5e2d6e2b8192061447629533cff2f1ef883124cf
Matt Hamers <development@matthamers.com>
2019-04-13 15:03:38 +0200

    finalized map

--- map.js
+++ map.js
@@ -1,16 +1,239 @@
 // STEP 1 create a funciton to initialize the map.
+function initMap() {
+  var myLatLng = { lat: 52.368, lng: 4.9036 };
+  var options = {
+    zoom: 12,
+    center: myLatLng,
+    disableDefaultUI: true,
+    styles: [
+      {
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#212121"
+          }
+        ]
+      },
+      {
+        elementType: "labels.icon",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#757575"
+          }
+        ]
+      },
+      {
+        elementType: "labels.text.stroke",
+        stylers: [
+          {
+            color: "#212121"
+          }
+        ]
+      },
+      {
+        featureType: "administrative",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#757575"
+          }
+        ]
+      },
+      {
+        featureType: "administrative.country",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#9e9e9e"
+          }
+        ]
+      },
+      {
+        featureType: "administrative.land_parcel",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        featureType: "administrative.locality",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#bdbdbd"
+          }
+        ]
+      },
+      {
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#757575"
+          }
+        ]
+      },
+      {
+        featureType: "poi.park",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#181818"
+          }
+        ]
+      },
+      {
+        featureType: "poi.park",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#616161"
+          }
+        ]
+      },
+      {
+        featureType: "poi.park",
+        elementType: "labels.text.stroke",
+        stylers: [
+          {
+            color: "#1b1b1b"
+          }
+        ]
+      },
+      {
+        featureType: "road",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            color: "#2c2c2c"
+          }
+        ]
+      },
+      {
+        featureType: "road",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#8a8a8a"
+          }
+        ]
+      },
+      {
+        featureType: "road.arterial",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#373737"
+          }
+        ]
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#3c3c3c"
+          }
+        ]
+      },
+      {
+        featureType: "road.highway.controlled_access",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#4e4e4e"
+          }
+        ]
+      },
+      {
+        featureType: "road.local",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#616161"
+          }
+        ]
+      },
+      {
+        featureType: "transit",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#757575"
+          }
+        ]
+      },
+      {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#000000"
+          }
+        ]
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#3d3d3d"
+          }
+        ]
+      }
+    ]
+  };
 
+  var map = new google.maps.Map(document.getElementById("map"), options);
 
-// STEP 2 add options to the map.
+  function addMarker(props) {
+    var marker = new google.maps.Marker({
+      position: props.coordinates,
+      map: map,
+      icon: props.icon
+    });
 
+    if (props.content) {
+      var infoWindow = new google.maps.InfoWindow({
+        content: props.content
+      });
+      marker.addListener("click", function() {
+        infoWindow.open(map, marker);
+      });
+    }
+  }
+
+  var markers = [
+    {
+      content: "<h1>Ironhack Campus</h1>",
+      coordinates: { lat: 52.341053, lng: 4.874422 },
+      icon: "images/logo-ironhack-blue.png"
+    },
+    {
+      content: "<img src='images/logo-ironhack-blue.png'></img>",
+      coordinates: { lat: 52.379189, lng: 4.899431 },
+      icon: "images/NS.png"
+    }
+  ];
+
+  for (let i = 0; i < markers.length; i++) {
+    addMarker(markers[i]);
+  }
+}
 
 // STEP 3 set first marker.
 
-
 // STEP 4 make function to add markers dynamically.
 
-
 // STEP 5 call marker function with an array of marker objects.
-
 
 // BONUS map styling
